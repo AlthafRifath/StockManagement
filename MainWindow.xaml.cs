1d6a9f55f1fd92e2696f66a8ec76beb230a2d186
Althaf <althafrifath@gmail.com>
2023-09-10 21:48:54 +0530

    Major Fix on Database Connection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,66 @@
-﻿using System.Windows;
+﻿using System;
+using System.Data;
+using System.Windows;
+using MySql.Data.MySqlClient;
 
 namespace StockManagement
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : Window
     {
+        private MySqlConnection connection;
+        // private string connectionString = "Server=localhost;Database=stockmanagementdb;User ID=admin;Password=1100";
+        private string connectionString = "server=localhost;user=root;database=stockmanagementdb;port=3306;password=1100";
+
         public MainWindow()
         {
             InitializeComponent();
+            connection = new MySqlConnection(connectionString);
         }
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                LoginResultText.Text = "Please enter username and password!";
+            }
+            else
+            {
+                // Create a parameterized query to avoid SQL injection
+                string query = "SELECT * FROM Users WHERE Username = @username AND Password = @password";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                // Add parameters to the query
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                try
+                {
+                    connection.Open();
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    if (dataTable.Rows.Count == 1)
+                    {
+                        MessageBox.Show("Login successful!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login failed!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
         }
     }
 }
